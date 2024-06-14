8d67fdab8ffe4383f170d2b35de91e1ef0da1b89
muratdayan <muratdayan359@gmail.com>
2024-06-14 15:50:40 +0300

    feat: onboarding pages adding in MainActivity

--- app/src/main/java/com/muratdayan/onboardingscreen/MainActivity.kt
+++ app/src/main/java/com/muratdayan/onboardingscreen/MainActivity.kt
@@ -6,21 +6,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.muratdayan.onboardingscreen.onboarding.OnBoardingScreen
+import com.muratdayan.onboardingscreen.onboarding.OnBoardingUtils
 import com.muratdayan.onboardingscreen.ui.theme.OnBoardingScreenTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val onBoardingUtils by lazy {
+        OnBoardingUtils(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,23 +40,45 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             OnBoardingScreenTheme {
-                ShowOnBoardingScreen()
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    if(onBoardingUtils.isOnBoardingComplete()){
+                        ShowHomeScreen()
+                    }else{
+                        ShowOnBoardingScreen()
+                    }
+                }
             }
         }
     }
-}
 
-@Composable
-private fun ShowOnBoardingScreen(){
-    val context =  LocalContext.current
-
-    Box(
-        modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.background)
-    ){
-        OnBoardingScreen {
-            Toast.makeText(context,"OnBoarding Completed",Toast.LENGTH_SHORT).show()
+    @Composable
+    private fun ShowHomeScreen(){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(text = "Home Screen", style = MaterialTheme.typography.headlineLarge)
         }
     }
+
+
+    @Composable
+    private fun ShowOnBoardingScreen(){
+        val scope = rememberCoroutineScope()
+
+        OnBoardingScreen {
+            onBoardingUtils.setOnBoardingComplete()
+            scope.launch {
+                setContent {
+                    ShowHomeScreen()
+                }
+            }
+        }
+    }
+
 }
+
 
