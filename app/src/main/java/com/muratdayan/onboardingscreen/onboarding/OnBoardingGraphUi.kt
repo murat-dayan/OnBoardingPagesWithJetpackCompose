e3febd8fe3ac5661c9fab1b2e949b8d1b46a433e
muratdayan <muratdayan359@gmail.com>
2024-06-14 14:29:46 +0300

    style: onboarding top section styled

---
+++ app/src/main/java/com/muratdayan/onboardingscreen/onboarding/OnBoardingGraphUi.kt
@@ -0,0 +1,80 @@
+package com.muratdayan.onboardingscreen.onboarding
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun OnBoardingGraphUi(onBoardingModel: OnBoardingModel) {
+
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.size(50.dp))
+
+        Image(
+            painter = painterResource(id = onBoardingModel.image),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(50.dp, 0.dp),
+            alignment = Alignment.Center
+        )
+
+        Spacer(modifier = Modifier.size(50.dp))
+
+        Text(
+            text = onBoardingModel.title,
+            modifier = Modifier.fillMaxWidth(),
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Text(
+            text = onBoardingModel.description,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp, 0.dp),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnBoardingGraphUiPreview1() {
+    OnBoardingGraphUi(onBoardingModel = OnBoardingModel.FirstPages)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnBoardingGraphUiPreview2() {
+    OnBoardingGraphUi(onBoardingModel = OnBoardingModel.SecondPages)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnBoardingGraphUiPreview3() {
+    OnBoardingGraphUi(onBoardingModel = OnBoardingModel.ThirdPages)
+}
--- app/src/main/java/com/muratdayan/onboardingscreen/onboarding/OnBoardingModel.kt
+++ app/src/main/java/com/muratdayan/onboardingscreen/onboarding/OnBoardingModel.kt
@@ -1,4 +1,28 @@
 package com.muratdayan.onboardingscreen.onboarding
 
-class OnBoardingModel {
+import androidx.annotation.DrawableRes
+import com.muratdayan.onboardingscreen.R
+
+sealed class OnBoardingModel(
+    @DrawableRes val image:Int,
+    val title:String,
+    val description:String
+){
+    data object FirstPages : OnBoardingModel(
+        image = R.drawable.img1,
+        title = "First Page",
+        description = "This is the first page description"
+    )
+
+    data object SecondPages : OnBoardingModel(
+        image = R.drawable.img2,
+        title = "Second Page",
+        description = "This is the second page description"
+    )
+
+    data object ThirdPages : OnBoardingModel(
+        image = R.drawable.img3,
+        title = "Third Page",
+        description = "This is the third page description"
+    )
 }
